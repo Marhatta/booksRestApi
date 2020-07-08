889d71e6680e6fec098b89921ebfabc9d183e26d
Marhatta <vishal.marhatta@gmail.com>
2020-07-09 00:13:13 +0530

    validated POST api

--- models/books.js
+++ models/books.js
@@ -1,23 +1,45 @@
-const mongoose = require('mongoose');
-const Author = require('./author');
-
+const mongoose = require("mongoose");
+const Author = require("./author");
+const yup = require("yup");
 
 //BOOK SCHEMA
 const BookSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        minlength:3,
-        maxlength:50
-    },
-    author:Author.schema,
-    genre:{
-        type:String,
-        required:true,
-        minlength:3,
-        maxlength:20
-    }
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  author: Author.schema,
+  genre: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 20,
+  },
 });
 
+const validateBook = (book) => {
+  const schema = yup.object().shape({
+    bookName: yup.string().required().min(3).max(50),
+    authorName: yup.string().required().min(3).max(40),
+    authorAge: yup
+      .number()
+      .required()
+      .min(10, "Age must be greater than 10")
+      .max(100, "Age must be less than 100"),
+    genre: yup.string().required().min(3).max(20),
+  });
 
-module.exports = new mongoose.model('Book',BookSchema);
+  return schema
+    .validate(book)
+    .then((book) => book)
+    .catch((error) => {
+      return {
+        message: error.message,
+      };
+    });
+};
+
+exports.Book = new mongoose.model("Book", BookSchema);
+exports.validateBook = validateBook;
