68e28a4e6adb97d614fa1ac39fd4a8d3f7191390
Marhatta <vishal.marhatta@gmail.com>
2020-07-16 02:42:31 +0530

    cors issue

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const booksRoute = require('./routes/books');
 const PORT = process.env.PORT || 3000;
 
 //middlewares
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors())
+
 
 //create a logger
 const logger = winston.createLogger({
