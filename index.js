4d26046d71b9fbf1199efa93422a826a201b081e
Marhatta <vishal.marhatta@gmail.com>
2020-07-16 01:41:12 +0530

    Added winston logger to handle errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const winston = require('winston');
 const app = express();
 require("dotenv").config();
 const booksRoute = require('./routes/books');
@@ -10,20 +11,37 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//create a logger
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console({
+      format:winston.format.combine(
+        winston.format.colorize({all:true})
+      )
+    }),
+    new winston.transports.File({ filename: 'error.log' ,level:'error'})
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'exceptions.log' })
+  ]
+});
+
 //routes
 app.use('/api/books',booksRoute);
+
 
 //connect to mongodb atlas
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
   .then(() => {
-    console.log("Connected to mongodb atlas");
+    logger.info("connected to mongoDb atlas");
   })
   .catch((error) => {
-    console.log("Something wring happened", error);
+    logger.error(error.message);
   });
 
 //start the server
 app.listen(PORT, () => {
-  console.log("Server started at PORT ", PORT);
+  logger.info(`Server started at PORT ${PORT}`);
 });
--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {Book, validateBook} = require("../models/books");
+const { Book, validateBook } = require("../models/books");
 
 //POST: CREATE A NEW BOOK
 router.post("/", async (req, res) => {
   const error = await validateBook(req.body);
-  if(error.message) res.status(400).send(error.message);
+  if (error.message) res.status(400).send(error.message);
 
   book = new Book({
     name: req.body.bookName,
@@ -26,5 +26,46 @@ router.post("/", async (req, res) => {
     });
 });
 
+//GET ALL BOOKS
+router.get("/", (req, res) => {
+  Book.find()
+    .then((books) => res.send(books))
+    .catch((error) => {
+      res.status(500).send("Something went wrong");
+    });
+});
+
+//GET THE BOOK BY ID
+router.get("/:bookId", async (req, res) => {
+  const book = await Book.findById(req.params.bookId);
+  if (!book) res.status(404).send("Book not found");
+  res.send(book);
+});
+
+//UPDATE BOOK BASED ON ID
+router.put("/:bookId", async (req, res) => {
+  const updatedBook = await Book.findByIdAndUpdate(
+    req.params.bookId,
+    {
+      name: req.body.bookName,
+      author: {
+        name: req.body.authorName,
+        age: req.body.authorAge,
+      },
+      genre: req.body.genre,
+    },
+    { new: true }
+  );
+
+  if (!updatedBook) res.status(404).send("book nont found");
+  res.send(updatedBook);
+});
+
+//DELETE BOOK BASED ON ID
+router.delete("/:bookId", async (req, res) => {
+  const book = await Book.findByIdAndRemove(req.params.bookId);
+  if (!book) res.status(404).send("book with id not found");
+  res.send(book);
+});
 
 module.exports = router;
